cc14f290dc019f46490a85a45d9c24d0a8c45ff7
andreyrehv <andreycaldas@gmail.com>
2018-10-30 15:34:05 -0300

    Parte direita Finalizada

--- src/aula/PrimeiraTela.java
+++ src/aula/PrimeiraTela.java
@@ -6,69 +6,119 @@ import javax.swing.JLabel;
 import javax.swing.JTextField;
 
 public class PrimeiraTela extends JFrame{
-	JLabel lblNome = new JLabel("Nome: ");
+	
+	//Parte esquerda
+	JLabel lblNome = new JLabel("NOME: ");
 	JTextField txtNome = new JTextField();
+	
+	JLabel lblEndereco = new JLabel("ENDEREÇO: ");
+	JTextField txtEndereco = new JTextField();
+	
+	JLabel lblCidade = new JLabel("CIDADE: ");
+	JTextField txtCidade = new JTextField();
+	
+	JLabel lblBairro = new JLabel("BAIRRO: ");
+	JTextField txtBairro = new JTextField();
+	
+	JLabel lblTelefone = new JLabel("TELEFONE: ");
+	JTextField txtTelefone = new JTextField();
+	
+	JLabel lblRG = new JLabel ("RG: ");
+	JTextField txtRG = new JTextField();
+	
+	//Parte direita
+	
+	
+	JLabel lblCelular = new JLabel("CELULAR: ");
+	JTextField txtCelular = new JTextField();
+	
+	JLabel lblCpf = new JLabel("CPF: ");
+	JTextField txtCpf = new JTextField();
+	
+	
+	
+	
+	
 	public PrimeiraTela() {
 		super("Titulo da Minha Janela");
 		
 		Container paine = this.getContentPane();   //Fixa os objetos no layout
 		
+		
+		//Nome
 		paine.add(lblNome);
-		lblNome.setBounds(10,20,100,20);
+		lblNome.setBounds(10,20,100,30);
 		
 		paine.add(txtNome);
-		txtNome.setBounds(50,20,220,20);
+		txtNome.setBounds(80,20,250,30);
 		
-		////////////////////////////////////////////////////
+		//Endereço 
+		paine.add(lblEndereco);
+		lblEndereco.setBounds(10,60,100,30);
+			
+		paine.add(txtEndereco);
+		txtEndereco.setBounds(80,60,250,30);
 		
-		JLabel lblCpf = new JLabel("CPF: ");
-		JTextField txtCpf = new JTextField();
-			
-			
-			paine.add(lblCpf);
-			lblCpf.setBounds(10,50,100,20);
-			
-			paine.add(txtCpf);
-			txtCpf.setBounds(50,50,220,20);
-			
-		////////////////////////////////////////////////////
-			
-		JLabel lblEndereco = new JLabel("Endereço: ");
-		JTextField txtEndereco = new JTextField();
+		//Cidade
+		paine.add(lblCidade);
+		lblCidade.setBounds(10,100,100,30);
 		
-			paine.add(lblEndereco);
-			lblEndereco.setBounds(10,80,100,20);
-			
-			paine.add(txtEndereco);
-			txtEndereco.setBounds(70,80,200,20);
+		paine.add(txtCidade);
+		txtCidade.setBounds(80,100,250,30);
 		
-		////////////////////////////////////////////////////
-			
-			JLabel lblTelefone = new JLabel("Telefone: ");
-			JTextField txtTelefone = new JTextField();
-			
-				paine.add(lblTelefone);
-				lblTelefone.setBounds(10,110,100,20);
+		//Bairro
+		paine.add(lblBairro);
+		lblBairro.setBounds(10,140,100,30);
+		
+		paine.add(txtBairro);
+		txtBairro.setBounds(80,140,250,30);
+		
+		//Telefone
+		paine.add(lblTelefone);
+		lblTelefone.setBounds(10,180,100,30);
 				
-				paine.add(txtTelefone);
-				txtTelefone.setBounds(70,110,200,20);
+		paine.add(txtTelefone);
+		txtTelefone.setBounds(80,180,100,30);
+		
+		//RG
+		paine.add(lblRG);
+		lblRG.setBounds(10,220,100,30);
+		
+		paine.add(txtRG);
+		txtRG.setBounds(80,220,100,30);
+		
+		////////////////////////////////////////////////////
+		
+	/*				
+		paine.add(lblCpf);
+		lblCpf.setBounds(10,60,100,30);
+			
+		paine.add(txtCpf);
+		txtCpf.setBounds(50,60,220,30);
+		
+		////////////////////////////////////////////////////
+			
+			
+		
+		
+		////////////////////////////////////////////////////
+			
+						
+		
 		
 			
 		////////////////////////////////////////////////////
 		
-				JLabel lblCelular = new JLabel("Celular: ");
-				JTextField txtCelular = new JTextField();
-				
-					paine.add(lblCelular);
-					lblCelular.setBounds(290,110,100,20);
+		paine.add(lblCelular);
+		lblCelular.setBounds(290,140,100,30);
 					
-					paine.add(txtCelular);
-					txtCelular.setBounds(340,110,200,20);
-				
-		////////////////////////////////////////////////////
+		paine.add(txtCelular);
+		txtCelular.setBounds(340,140,200,30);
+			
+		*////////////////////////////////////////////////////
 			
 		this.setLayout(null);
-		this.setSize(600, 400);
+		this.setSize(600,320);
 		//this.setResizable(0);
 		this.setVisible(true);
 		
