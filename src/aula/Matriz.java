4f343247eb63766c624d11b10d07e49fdbf70d1c
andreyrehv <andreycaldas@gmail.com>
2018-11-27 17:31:44 -0200

    Revisão

---
+++ src/aula/Matriz.java
@@ -0,0 +1,18 @@
+package aula;
+
+public class Matriz {
+	
+public static void main (String[] args) {
+		
+		int []  linha 1= new int [3];
+		
+		linha1[0]= 1;
+		linha1[1]= 10;
+		linha1[2]= 20;
+		
+		for (int i=0; i<linha1.length; i++) {
+			System.out.println(linha1[1]);
+			
+		}
+}
+}
